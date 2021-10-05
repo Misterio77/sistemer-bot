94a58b9a025ef66d475e01e1243aef69a3f56fcf
Gabriel Fontes <eu@misterio.me>
2021-10-05 10:28:59 -0300

    consertar app do flake

--- flake.nix
+++ flake.nix
@@ -8,14 +8,17 @@
     flake-compat.flake = false;
   };
   outputs = { self, nixpkgs, flake-utils, flake-compat }:
-  flake-utils.lib.eachDefaultSystem (system: 
+  flake-utils.lib.eachDefaultSystem (system:
     let
       name = "sistemer-bot";
       pkgs = (import nixpkgs { inherit system; });
     in rec {
       packages.${name} = (import ./Cargo.nix { inherit pkgs; }).rootCrate.build;
       defaultPackage = packages.${name};
-      apps.${name} = flake-utils.lib.mkApp { drv = packages.${name}; };
+      apps.${name} = {
+        type = "app";
+        program = "${packages.${name}}/bin/${name}";
+      };
       defaultApp = apps.${name};
       devShell = pkgs.mkShell {
         buildInputs = with pkgs;
