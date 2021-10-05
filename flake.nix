e2d9700acc33d33534b63b2cfb1c9f0fba6a247b
Gabriel Fontes <eu@misterio.me>
2021-10-05 09:03:59 -0300

    usar crate2nix ao invés do naersk, por builds incrementais

--- flake.nix
+++ flake.nix
@@ -4,41 +4,31 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    naersk.url = "github:nix-community/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
-
-  outputs = { self, nixpkgs, flake-utils, naersk }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        name = "sistemer-bot";
-        pkgs = nixpkgs.legacyPackages.${system};
-        naersk-lib = naersk.lib."${system}";
-      in rec {
-        packages.${name} = naersk-lib.buildPackage {
-          pname = name;
-          buildInputs = with pkgs; [ openssl pkg-config ];
-          root = ./.;
-        };
-        defaultPackage = packages.${name};
-
-        apps.${name} = flake-utils.lib.mkApp {
-          drv = packages.${name};
-        };
-        defaultApp = apps.${name};
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              rustc
-              cargo
-              rust-analyzer
-              rustfmt
-              clippy
-              pkgconfig
-              openssl
-            ];
-        };
-      });
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+  flake-utils.lib.eachDefaultSystem (system: 
+    let
+      name = "sistemer-bot";
+      pkgs = (import nixpkgs { inherit system; });
+    in rec {
+      packages.${name} = (import ./Cargo.nix { inherit pkgs; }).rootCrate.build;
+      defaultPackage = packages.${name};
+      apps.${name} = flake-utils.lib.mkApp { drv = packages.${name}; };
+      defaultApp = apps.${name};
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [
+            rustc
+            cargo
+            rust-analyzer
+            rustfmt
+            clippy
+            pkgconfig
+            openssl
+          ];
+      };
+    }
+  );
 }
